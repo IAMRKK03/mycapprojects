b6cba3cd2aeff1abaeb8bd5b284fcf350b3ffdc6
R KAMLESH KUMAR <61360209+IAMRKK03@users.noreply.github.com>
2021-01-09 14:28:56 +0530

    Add files via upload
    Python code to calculate Area of Circle

---
+++ project.py
@@ -0,0 +1,5 @@
+#Program To Calculate the Area Of Circle
+from math import*
+radius=float(input(" what is the radius of the circle: ")) #asking the user to input the radius of the circle
+area=pi*radius**2 #calculating the area of the circle
+print(" The area of the circle is " + str(area)) #displaying the area calculated with the given radius of circle
